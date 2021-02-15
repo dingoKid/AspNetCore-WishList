e336dd668e46bf828be0057175e47e9c9d2656ce
unknown <gyetvaid@gmail.com>
2021-02-15 18:30:22 +0100

    Configuration added

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -29,7 +29,7 @@ namespace WishList
             }
             else
             {
-                app.UseExceptionHandler("/home/error");
+                app.UseExceptionHandler("/Home/Error");
             }
 
             app.UseRouting();
